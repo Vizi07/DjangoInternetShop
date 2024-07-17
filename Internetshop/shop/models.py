63e3d9bb084ba5c17e547ff680f0a529ff2a6340
Vizi07 <stamdemt@gmail.com>
2024-07-17 20:32:12 +0300

    Добавил модель для админов Добавил админа

--- Internetshop/shop/models.py
+++ Internetshop/shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.IntegerField()
+    image_Url = models.CharField(max_length=256)
