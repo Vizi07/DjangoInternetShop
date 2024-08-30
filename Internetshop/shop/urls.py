6ddada37030ed4c031df30af4a8f8e18384231fb
Vizi07 <stamdemt@gmail.com>
2024-08-30 20:29:36 +0300

    Реколор и ребрендинг

--- Internetshop/shop/urls.py
+++ Internetshop/shop/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.home, name="home"),
-	path("product/<int:id>", views.view_product, name="view_product"), # страница
-	path("payment/<int:id>", views.payment, name="payment"),
-	path("success", views.payment_success, name="success")
+    path("", views.home, name="home"),
+    path("product/<int:id>", views.view_product, name="view_product"),  # страница
+    path("payment/<int:id>", views.payment, name="payment"),
+    path("success", views.payment_success, name="success")
 ]
